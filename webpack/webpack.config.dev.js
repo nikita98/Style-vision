57c22b1adcba406fae14be7244684d55badbcef5
Никита Зуев <nikita_zuev_98@mail.ru>
2020-11-23 23:59:02 +0800

    first commit

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,62 @@
+const Path = require("path");
+const Webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "cheap-eval-source-map",
+  output: {
+    chunkFilename: "js/[name].chunk.js",
+  },
+  devServer: {
+    inline: true,
+    hot: true,
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+    new StylelintPlugin({
+      files: Path.join("src", "**/*.s?(a|c)ss"),
+    }),
+    new Webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, "../src"),
+        enforce: "pre",
+        loader: "eslint-loader",
+        options: {
+          emitWarning: true,
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, "../src"),
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          "style-loader",
+          "css-loader?sourceMap=true",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+});
